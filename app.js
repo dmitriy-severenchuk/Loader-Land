e0b4a6e43a0a64094e422abcdc397e17a4db3877
Dmitriy <severenchuk.dmitriy@point2web.com>
2021-09-20 19:37:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+let counter = 0;
+let a = 0;
+let counterPercent = document.getElementById('counter');
+let progressBar = document.getElementById('progressBar');
+let title = document.getElementById('title');
+let reviews = document.querySelectorAll('#review');
+let startButton = document.getElementById('startButton');
+let main = document.getElementById('main');
+let startBlock = document.getElementById('startBlock');
+let finallBlock = document.getElementById('finally');
+//START
+
+startButton.addEventListener('click', () => {
+  startBlock.classList.add('--animationRemove');
+  setTimeout(() => {
+    startBlock.classList.add('--remove');
+  }, 600);
+  new Promise((res) => {
+    setTimeout(() => {
+      main.classList.add('--show');
+      const i = setInterval(function () {
+        reviews[0].classList.add('--showReview');
+
+        counterPercent.innerHTML = a + '%';
+        progressBar.style.width = a + '%';
+
+        if (a >= 22) {
+          title.innerHTML = 'Checking Credit Score & Driving Records';
+          reviews[1].classList.add('--showReview');
+        }
+        if (a >= 50) {
+          counterPercent.style.color = 'white';
+        }
+        if (a >= 60) {
+          reviews[2].classList.add('--showReview');
+        }
+        if (a >= 100) {
+          title.innerHTML = 'Checking Eligibility';
+        }
+        counter++;
+        a++;
+        if (counter == 101) {
+          res(clearInterval(i));
+        }
+        return counter;
+      }, 100);
+    }, 800);
+  }).then(() => {
+    setTimeout(() => {
+      main.classList.add('--animationRemove');
+    }, 1300);
+    setTimeout(() => {
+      main.classList.remove('--show');
+    }, 1700);
+    setTimeout(() => {
+      finallBlock.classList.add('--show');
+    }, 1600);
+  });
+});
