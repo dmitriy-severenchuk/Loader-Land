ff84dcb37aa0da84377170bb56b2b9d95b777765
Dmitriy <severenchuk.dmitriy@point2web.com>
2021-09-22 14:39:08 +0300

    afaf

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ let touchendY = 0
 const slider = document.getElementById('swipe')
 
 function handleGesture() {
-  if (touchendY + 100 < touchstartY) alert('swiped!');
+  if (touchendY + 60 < touchstartY) alert('swiped!');
 }
 
 slider.addEventListener('touchstart', e => {
